e2aedd4ae8100ed4db45ab9cecc1fc6ac2870150
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-29 14:42:58 -0700

    add logic to calculate cost.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business logic goes here
 function Pizza(size, sauce, toppings) {
-  this.sauce = sauce;
   this.size = size;
+  this.sauce = sauce;
   this.toppings = toppings;
 }
 
@@ -10,7 +10,35 @@ Pizza.prototype.fullDisplay = function() {
 }
 
 Pizza.prototype.calculateCost = function() {
-
+  var sizeCost;
+  var sauceCost = 2;
+  var toppingsMeat = 0;
+  var toppingsVeg = 0;
+  var toppingsOther = 0;
+  if (this.size === "small") {
+    sizeCost = 5;
+  } else if (this.size === "medium") {
+    sizeCost = 7;
+  } else if (this.size === "large") {
+    sizeCost = 10;
+  }
+  for (var i = 0; i < this.toppings.length; i++) {
+    if (this.toppings[i] === "pepperoni" || "sausage" || "ham" || "bacon") {
+      toppingsMeat +=1.25;
+    } else {
+      toppingsMeat = 0;
+    }
+    if (this.toppings[i] === "bell peppers" || "mushrooms" || "onion" || "artichoke" || "olives") {
+      toppingsVeg +=1;
+    } else {
+      toppingsVeg = 0;
+    }
+    if (this.toppings[i] === "sardines" || "garlic" || "pineapple" ) {
+      toppingsOther +=0.5;
+    } else {
+      toppingsOther = 0;
+    }
+  } return sizeCost + sauceCost + toppingsMeat + toppingsVeg + toppingsOther;
 }
 
 
@@ -22,16 +50,16 @@ $(document).ready(function() {
    var name = $("#name").val();
    var size = $("#pizzaSize").val();
    var sauce = $("#pizzaSauce").val();
-   var toppings = [];
-   $("input:checkbox[name=pizzaToppings]:checked").each(function(){
-     toppings.push($(this.val()));
-   })
+   var toppings = $("input:checkbox[name=pizzaToppings]:checked").map(function(){
+     return $(this).val();
+   }).get();
    var pizzaItem = new Pizza(size, sauce, toppings);
    var pizzaDisplay = pizzaItem.fullDisplay();
+   var pizzaCost = pizzaItem.calculateCost();
    $(".order").show();
    $(".displayName").text(name);
    $(".result").text(pizzaDisplay);
-   $(".cost").text();
+   $(".cost").text(pizzaCost);
 
   });
 });
