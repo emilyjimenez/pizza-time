96e22c8732ba6f2d488524b3f296452f853dd4c2
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-29 12:53:31 -0700

    create html form, define specs, build initial business/ui logic to display results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
 // Business logic goes here
+function Pizza(size, sauce, toppings) {
+  this.sauce = sauce;
+  this.size = size;
+  this.toppings = [];
+}
 
+Pizza.prototype.fullDisplay = function() {
+  return ", You ordered a " + this.size + " pizza, with" + this.sauce + ", and these toppings: " + this.toppings;
+}
 
+Pizza.prototype.calculateCost = function() {
 
+}
 
 
 
 // UI logic goes here
 $(document).ready(function() {
+  $("#pizzaForm").submit(function(event) {
+   event.preventDefault();
+   var name = $("#name").val();
+   var size = $("#pizzaSize").val();
+   var sauce = $("#pizzaSauce").val();
+   var pizzaItem = new Pizza(movie, time, age);
+   var pizzaDisplay = pizzaItem.fullDisplay();
+   $(".result").text(name + pizzaDisplay);
+   $(".cost").text();
 
   });
 });
